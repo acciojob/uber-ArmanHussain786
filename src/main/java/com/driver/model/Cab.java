9e95aa642e7b7b0feecd6f08c67496f987a94f2e
Arman Hussain <mail2arman2001@gmail.com>
2023-04-26 23:24:11 +0530

    yo

--- src/main/java/com/driver/model/Cab.java
+++ src/main/java/com/driver/model/Cab.java
@@ -12,7 +12,7 @@ public class Cab {
 
     private int perKmRate;
 
-    private boolean available;
+    private boolean Available;
 
     @OneToOne
     @JoinColumn
@@ -39,11 +39,11 @@ public class Cab {
 
 
     public boolean isAvailable() {
-        return available;
+        return Available;
     }
 
     public void setAvailable(boolean available) {
-        this.available = available;
+        Available = available;
     }
 
     public Driver getDriver() {
